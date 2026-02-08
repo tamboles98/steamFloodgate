1b3ee94250bbb2b2648033aa9b1bc0d24914cf6f
Santiago Cebellan <tamboles98@gmail.com>
2026-02-08 23:04:39 +0100

    Temporal: Añadir listado de juegos de prueba

--- packages/front/src/routes/result.tsx
+++ packages/front/src/routes/result.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Game } from "../components/Game";
 
 export const Route = createFileRoute("/result")({
   component: RouteComponent,
@@ -7,7 +8,32 @@ export const Route = createFileRoute("/result")({
 function RouteComponent() {
   return (
     <div>
-      <p>Aquí es donde tendremos los resultados cuando los implementemos</p>
+      <ul>
+        <li>
+          <Game
+            name="Team Fortress 2"
+            hoursPlayed={33365}
+            appId={440}
+            imageIcon="f568912870a4684f9ec76277a1a404dda6bab213"
+          />
+        </li>
+        <li>
+          <Game
+            name="Team Fortress 2"
+            hoursPlayed={33365}
+            appId={440}
+            imageIcon="f568912870a4684f9ec76277a1a404dda6bab213"
+          />
+        </li>
+        <li>
+          <Game
+            name="Team Fortress 2"
+            hoursPlayed={33365}
+            appId={440}
+            imageIcon="f568912870a4684f9ec76277a1a404dda6bab213"
+          />
+        </li>
+      </ul>
     </div>
   );
 }
