4322f10c33eec21eb88f150473a2d7f2bd0c572e
Santiago Cebellan <tamboles98@gmail.com>
2026-02-11 18:27:01 +0100

    Arreglar configuraciones de eslint

--- packages/back/eslint.config.js
+++ packages/back/eslint.config.js
@@ -1,14 +1,19 @@
 import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: { globals: globals.node },
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
   },
   tseslint.configs.recommended,
 ]);
